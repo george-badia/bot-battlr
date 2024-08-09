85334de7c642bebd905f60d8341541a79db9579c
jojo <georgebadia2019@gmail.com>
2024-08-09 14:50:18 +0300

    adds bot army function,bot army events and display enlisted bot

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app-container">
+      <YourBotArmy />
       <BotCollection />
     </div>
   );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import BotCard from "./BotCard";
+import "./YourBotArmy.css";
 
-const YourBotArmy = () => {
+const YourBotArmy = ({
+  enlistedBots,
+  releaseBotHandler,
+  dischargeBotHandler,
+}) => {
   return (
-    <div>YourBotArmy</div>
-  )
-}
+    <>
+      <div>
+        <h2 className="your-bot-army-title">Your Bot Army</h2>
+      </div>
+      <div className="your-bot-army-container">
+        {enlistedBots.map((bot) => (
+          <BotCard
+            key={bot.id}
+            botData={bot}
+            onBotClick={releaseBotHandler}
+            onDischargeClick={dischargeBotHandler}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default YourBotArmy
+export default YourBotArmy;
