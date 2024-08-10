17315d7cae9fc7c7666099fa770c0afcb94b6eec
jojo <georgebadia2019@gmail.com>
2024-08-11 01:42:14 +0300

    updates bot list

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,18 +1,16 @@
+export default BotCollection;
 import React from "react";
 import BotCard from "./BotCard";
 import "./BotCollection.css";
-
 // Function component for the BotCollection
 const BotCollection = ({ botsList, enlistBotHandler }) => {
   return (
     <>
       <div>
-        <h2 className="bot-collection-container-title">Bot Collection</h2>
+        <h2 className="bot-collection-container-title"> Bot Collection</h2>
       </div>
-
       <div className="bot-collection-container">
         {/* Map through botsList and render BotCard for each robot */}
-
         {botsList.map((bot) => (
           <BotCard key={bot.id} botData={bot} onBotClick={enlistBotHandler} />
         ))}
